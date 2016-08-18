87a0b1ee7c58817e64e0349c80fd3e0226201e25
Chun Tai <chuntai@kkboxs-macbook-pro-3.local>
2016-08-18 11:38:12 +0800

    Initial Commit

---
+++ KKBOX_Personal Porfile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KKBOX_Personal Porfile
+//
+//  Created by Chun Tai on 2016/8/18.
+//  Copyright © 2016年 Chun Tai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
