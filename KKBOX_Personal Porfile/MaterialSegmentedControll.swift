79066e68eb574075a1fac08de479bfe05cba247b
Chun-Tie,Lin <style831211@gmail.com>
2016-08-28 14:48:35 +0800

    Try merge final

---
+++ KKBOX_Personal Porfile/MaterialSegmentedControll.swift
@@ -0,0 +1,18 @@
+//
+//  MaterialSegmentedControll.swift
+//  KKBox_People_Project
+//
+//  Created by 辛忠翰 on 2016/8/14.
+//  Copyright © 2016年 Keynote. All rights reserved.
+//
+
+import UIKit
+
+class MaterialSegmentedControll: UISegmentedControl {
+    //let mySegmentedControl = UISegmentedControl()
+    override func awakeFromNib() {
+        //self.tintColor =  UIColor.whiteColor()
+       // self.backgroundColor = UIColor.clearColor()
+        
+    }
+}
